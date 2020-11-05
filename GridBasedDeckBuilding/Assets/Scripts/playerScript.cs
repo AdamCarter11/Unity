c8e8e019e06939e7e321a1da501534b35f2d823f
Adam <57573881+AdamCarter11@users.noreply.github.com>
2020-11-04 17:08:34 -0800

    Grid based movement
    Set up 2D grid based movement so I could use it in games like snake, or turn based games, etc.

---
+++ GridBasedDeckBuilding/Assets/Scripts/playerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    public Transform movePoint;
+    public LayerMask stopsMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //we keep movepoint as a child, but when game runs it isn't a child so we can move it
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed*Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal"))==1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, stopsMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            //If I want diagonal, get rid of else
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, stopsMovement))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+                   
+            }
+        }
+
+        
+    }
+
+}
